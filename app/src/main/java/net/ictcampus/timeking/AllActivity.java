d3ce564ab4c373a036707002e59c37fe60bbe12a
zauggmo <moritz.zaugg@ict-campus.net>
2019-05-22 15:00:12 +0200

    Gui designing

--- app/src/main/java/net/ictcampus/timeking/AllActivity.java
+++ app/src/main/java/net/ictcampus/timeking/AllActivity.java
@@ -10,4 +10,5 @@ public class AllActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all);
     }
+
 }
--- app/src/main/java/net/ictcampus/timeking/MainActivity.java
+++ app/src/main/java/net/ictcampus/timeking/MainActivity.java
@@ -2,16 +2,18 @@ package net.ictcampus.timeking;
 
 import android.content.ClipData;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, AddActivity.class));
             }
         });
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.action_All) {
+                    startActivity(new Intent(MainActivity.this, AddActivity.class));
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
 
 
